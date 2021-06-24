9b14467478c794a1d5c727b078bf24e066cb9035
Jose Ricardo Bettine Pacola (Terceiros) <trc6018760@unimedbh.com.br>
2021-06-24 17:03:13 -0300

    Inclusão de filtro de user_agent ; remoção de arquivos de teste

--- downloadCRM.py
+++ downloadCRM.py
@@ -10,28 +10,9 @@ from time import sleep
 import csv
 from random import randint
 
-# class TestTeste():
-#   def setup_method(self, method):
-#     self.driver = webdriver.Chrome()
-#     self.vars = {}
-  
-#   def teardown_method(self, method):
-#     self.driver.quit()
-  
-#   def test_teste(self):
-#     self.driver.get("https://portal.cfm.org.br/busca-medicos/")
-#     self.driver.set_window_size(1061, 701)
-#     self.driver.find_element(By.ID, "uf").click()
-#     dropdown = self.driver.find_element(By.ID, "uf")
-#     dropdown.find_element(By.XPATH, "//option[. = 'MG']").click()
-#     self.driver.find_element(By.ID, "uf").click()
-#     self.driver.find_element(By.XPATH, "//*[@id=\"buscaForm\"]/div/div[4]/div[2]/button").click()
-#     results = self.driver.find_elements_by_class_name("busca-resultado")
-#     for i in results:
-#       print(i.text)
 
 def setup():
-  userAgent = generate_user_agent()
+  userAgent = generate_user_agent(device_type="desktop")
 
   options = Options()
   options.add_argument('--headless')
